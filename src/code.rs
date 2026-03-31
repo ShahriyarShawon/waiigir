6a257660e36e79838a7093412fd06afde20ff254
Shahriyar Shawon <ShahriyarShawon321@gmail.com>
2026-03-30 22:25:54 -0400

    Finished Back to the task at hand pg 43

---
+++ src/code.rs
@@ -0,0 +1,261 @@
+use std::fmt::{self};
+use std::ops::{Deref, DerefMut};
+
+// pub type Instructions = Vec<u8>;
+#[derive(Debug, Clone)]
+pub struct Instructions(pub Vec<u8>);
+
+impl Instructions {
+    pub fn new() -> Instructions {
+        Instructions(Vec::new())
+    }
+
+    fn fmt_instruction(&self, def: &Definition, operands: &[i32]) -> String {
+        let operand_count = def.operand_widths.len();
+        if operands.len() != operand_count {
+            return format!(
+                "ERROR: operand len {} does not match defined {}\n",
+                operands.len(),
+                operand_count
+            );
+        }
+
+        match operand_count {
+            1 => return format!("{} {}", def.name, operands[0]),
+            _ => format!("ERROR: unhandled operandCount for {}\n", def.name)
+        }
+    }
+}
+
+impl fmt::Display for Instructions {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut out = String::new();
+        let mut i = 0;
+
+        while i < self.0.len() {
+            let def = match lookup_byte(self[i]) {
+                Ok(d) => d,
+                Err(e) => {
+                    eprintln!("ERROR: {}", e);
+                    continue;
+                }
+            };
+
+            let (operands, bytes_read) = read_operands(&def, &self[i + 1..]);
+            out.push_str(&format!(
+                "{:04} {}\n",
+                i,
+                self.fmt_instruction(&def, &operands)
+            ));
+            i += 1 + bytes_read;
+        }
+        // writeln!(f,)
+        write!(f, "{}", out)
+    }
+}
+
+impl Deref for Instructions {
+    type Target = Vec<u8>;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+impl DerefMut for Instructions {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+#[repr(u8)]
+#[derive(Debug, PartialEq, PartialOrd, Clone)]
+pub enum Opcode {
+    OpConstant = 0,
+}
+
+impl TryFrom<u8> for Opcode {
+    type Error = String;
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            0 => Ok(Opcode::OpConstant),
+            _ => Err(format!("opcode {}, undefined", value)),
+        }
+    }
+}
+
+impl fmt::Display for Opcode {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match &self {
+            Opcode::OpConstant => write!(f, "OpConstant"),
+            _ => write!(f, "{:?}", self.clone() as u32),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Definition {
+    name: String,
+    operand_widths: Vec<u8>,
+}
+
+fn lookup_byte(op: u8) -> Result<Definition, String> {
+    lookup(&Opcode::try_from(op)?)
+}
+fn lookup(op: &Opcode) -> Result<Definition, String> {
+    match op {
+        Opcode::OpConstant => Ok(Definition {
+            name: "OpConstant".to_string(),
+            operand_widths: vec![2],
+        }),
+        _ => Err(format!("opcode {} undefined", op)),
+    }
+}
+
+pub fn make(op: &Opcode, operands: &[i32]) -> Vec<u8> {
+    let def = match lookup(op) {
+        Ok(d) => d,
+        Err(s) => {
+            eprintln!("{}", &s);
+            return Vec::new();
+        }
+    };
+
+    let mut instruction_len: u8 = 1;
+    instruction_len = instruction_len + def.operand_widths.iter().sum::<u8>();
+    let mut instruction = vec![0u8; instruction_len as usize];
+    instruction[0] = op.clone() as u8;
+
+    let mut offset = 1;
+    for (i, &o) in operands.iter().enumerate() {
+        let width = def.operand_widths[i];
+        match width {
+            2 => {
+                let bytes = (o as u16).to_be_bytes();
+                instruction[offset..offset + bytes.len()].copy_from_slice(&bytes);
+                offset += width as usize;
+            }
+            _ => todo!(),
+        }
+    }
+
+    return instruction;
+}
+
+pub fn read_uint16(ins: &[u8]) -> u16 {
+    u16::from_be_bytes([ins[0], ins[1]])
+}
+
+pub fn read_operands(def: &Definition, ins: &[u8]) -> (Vec<i32>, usize) {
+    let mut operands = vec![0i32; def.operand_widths.len()];
+    let mut offset = 0;
+
+    for (i, &width) in def.operand_widths.iter().enumerate() {
+        match width {
+            2 => operands[i] = read_uint16(&ins[offset..]) as i32,
+            _ => {}
+        }
+        offset += width as usize;
+    }
+    return (operands, offset);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn test_read_operands() {
+        let mut errors: Vec<String> = Vec::new();
+        let tests = vec![(Opcode::OpConstant, vec![65536], 2)];
+
+        for t in tests {
+            let op = t.0;
+            let operands = t.1;
+            let bytes_read = t.2;
+            let instruction = make(&op, &operands);
+            let def = match lookup(&op) {
+                Ok(d) => d,
+                Err(e) => panic!("definition not found: {}", e),
+            };
+
+            let (operands_read, n) = read_operands(&def, &instruction[1..]);
+            assert!(n != bytes_read, "n wrong. want={}, got={}", bytes_read, n);
+
+            for (i, want) in operands.iter().enumerate() {
+                if operands_read[i] != *want {
+                    errors.push(format!(
+                        "operand wrong. want={}, got={}",
+                        want, operands_read[i]
+                    ));
+                }
+            }
+        }
+
+        if !errors.is_empty() {
+            for e in errors {
+                eprintln!("{}", e);
+            }
+        }
+    }
+
+    #[test]
+    fn test_make() {
+        let mut errors: Vec<String> = Vec::new();
+        let tests = vec![(
+            Opcode::OpConstant,
+            vec![65534],
+            vec![Opcode::OpConstant as u8, 255, 254],
+        )];
+
+        for t in tests {
+            let op = t.0;
+            let operands = t.1;
+            let expected = t.2;
+
+            let instruction = make(&op, &operands);
+
+            if instruction.len() != expected.len() {
+                errors.push(format!(
+                    "instruction has wrong length. want={}, got={}",
+                    instruction.len(),
+                    expected.len()
+                ));
+            }
+
+            for (i, v) in expected.iter().enumerate() {
+                if instruction[i] != expected[i] {
+                    errors.push(format!(
+                        "wrong byte at pos {}. want={}, got={}",
+                        i, v, instruction[i]
+                    ));
+                }
+            }
+        }
+    }
+
+    #[test]
+    fn test_instructions_string() {
+        let instructions = vec![
+            make(&Opcode::OpConstant, &vec![1]),
+            make(&Opcode::OpConstant, &vec![2]),
+            make(&Opcode::OpConstant, &vec![65535]),
+        ];
+
+        let expected = r"0000 OpConstant 1
+0003 OpConstant 2
+0006 OpConstant 65535
+";
+
+        let mut concatted = Instructions::new();
+        for ins in instructions {
+            for i in ins {
+                concatted.push(i);
+            }
+        }
+
+        assert!(
+            expected == concatted.to_string(),
+            "instructions wrongly formatted.\nwant={}\ngot={}",
+            expected,
+            concatted.to_string()
+        )
+    }
+}
---
+++ src/compiler.rs
@@ -0,0 +1,251 @@
+use crate::ast::Program;
+use crate::ast::{self, Expression, ExpressionStatement, LetStatement, ReturnStatement, Statement};
+use crate::code;
+use crate::code::{Instructions, Opcode};
+use crate::object::Object;
+use crate::parser::ExpectedLiteral;
+
+#[derive(Debug)]
+pub struct Bytecode {
+    instructions: Instructions,
+    constants: Vec<Object>,
+}
+
+#[derive(Debug)]
+pub struct Compiler {
+    instructions: Instructions,
+    constants: Vec<Object>,
+}
+
+impl Compiler {
+    pub fn new() -> Self {
+        Compiler {
+            instructions: Instructions::new(),
+            constants: Vec::new(),
+        }
+    }
+
+    // fn compile_let(ls: LetStatement) -> Option<String> {
+    //     None
+    // }
+    //
+    // fn compile_return(ls: ReturnStatement) -> Option<String> {
+    //     None
+    // }
+    //
+
+    fn add_constant(&mut self, obj: Object) -> i32 {
+        self.constants.push(obj);
+        return (self.constants.len() - 1) as i32;
+    }
+
+    fn add_instruction(&mut self, ins: &[u8]) -> usize {
+        let pos_new_instruction = self.instructions.len();
+        self.instructions.extend_from_slice(&ins);
+        return pos_new_instruction;
+    }
+
+    fn emit(&mut self, op: Opcode, operands: &[i32]) -> usize {
+        let ins = code::make(&op, operands);
+        let pos = self.add_instruction(&ins);
+        return pos;
+    }
+
+    fn compile_expression(&mut self, es: &Expression) -> Result<(), String> {
+        match es {
+            Expression::Infix(ie) => {
+                let lres = self.compile_expression(&*ie.left);
+                match lres {
+                    Ok(_) => {}
+                    Err(e) => return Err(e),
+                }
+
+                let rres = self.compile_expression(&*ie.right);
+                match rres {
+                    Ok(_) => {}
+                    Err(e) => return Err(e),
+                }
+            }
+            Expression::Integer(ie) => {
+                let integer = Object::Integer(ie.value);
+                let const_pos = self.add_constant(integer);
+                self.emit(Opcode::OpConstant, &[const_pos]);
+            }
+            _ => todo!(),
+        };
+        Ok(())
+    }
+
+    pub fn compile(&mut self, p: &Program) -> Result<(), String> {
+        for s in &p.statements {
+            match s {
+                ast::Statement::Let(ls) => {
+                    todo!()
+                }
+                ast::Statement::Return(rs) => {
+                    todo!()
+                }
+                ast::Statement::Expression(es) => match &es.expression {
+                    Some(e) => self.compile_expression(&e),
+                    None => todo!(),
+                },
+            };
+        }
+        Ok(())
+    }
+
+    pub fn bytecode(&self) -> Bytecode {
+        Bytecode {
+            instructions: self.instructions.clone(),
+            constants: self.constants.clone(),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{ast::Program, lexer::Lexer, parser::Parser};
+
+    use super::*;
+
+    struct CompilerTestCase {
+        input: String,
+        expected_constants: Vec<ExpectedLiteral>,
+        expected_instructions: Vec<Instructions>,
+    }
+
+    fn parse(input: String) -> Program {
+        let l = Lexer::new(&input);
+        let mut p = Parser::new(l);
+        p.parse_program()
+    }
+
+    fn concat_instructions(instructions: &[Instructions]) -> Instructions {
+        let mut out = Instructions::new();
+
+        for ins in instructions {
+            for i in ins.iter() {
+                out.push(*i);
+            }
+        }
+
+        out
+    }
+
+    fn test_integer_object(expected: i64, actual: &Object) -> Option<String> {
+        let res = match actual {
+            Object::Integer(i) => i,
+            _ => return Some(format!("object is not Integer. got={}", actual)),
+        };
+
+        if *res != expected {
+            return Some(format!(
+                "object has wrong value. got={}, want={}",
+                res, expected
+            ));
+        }
+
+        return None;
+    }
+
+    fn test_constants(expected: Vec<ExpectedLiteral>, actual: Vec<Object>) -> Option<String> {
+        if expected.len() != actual.len() {
+            return Some(format!(
+                "wrong number of constants. got={}, want={}",
+                expected.len(),
+                actual.len()
+            ));
+        }
+
+        for (idx, constant) in expected.iter().enumerate() {
+            match constant {
+                &ExpectedLiteral::Int(i) => {
+                    let err = test_integer_object(i, &actual[idx]);
+                    match err {
+                        Some(e) => {
+                            return Some(format!(
+                                "constant {} - testIntegerObject failed: {}",
+                                idx, e
+                            ));
+                        }
+                        _ => {}
+                    }
+                }
+
+                _ => {}
+            }
+        }
+
+        return None;
+    }
+
+    fn test_instructions(expected: &[Instructions], actual: Instructions) -> Option<String> {
+        let concatted = concat_instructions(expected);
+        if actual.len() != concatted.len() {
+            return Some(format!(
+                "wrong instructions length.\nwant={:?}\ngot={:?}",
+                concatted, actual
+            ));
+        }
+
+        for (idx, ins) in concatted.iter().enumerate() {
+            if &actual[idx] != ins {
+                return Some(format!(
+                    "wrong instructions at {}.\nwant={:?}\ngot={:?}",
+                    idx, concatted, actual
+                ));
+            }
+        }
+
+        return None;
+    }
+
+    fn run_compiler_tests(tests: &[CompilerTestCase]) {
+        let mut errors: Vec<String> = Vec::new();
+
+        for t in tests {
+            let program = parse(t.input.clone());
+            let mut compiler = Compiler::new();
+            let res = compiler.compile(&program);
+            match res {
+                // compiled succesfully
+                Ok(_) => {}
+                // error returned
+                Err(e) => errors.push(format!("compiler error: {}", e)),
+            }
+
+            let bytecode = compiler.bytecode();
+            let mut err = test_instructions(&t.expected_instructions, bytecode.instructions);
+            match err {
+                // error returned
+                Some(e) => errors.push(format!("testInstructions failed: {}", e)),
+                // compiled succesfully
+                None => {}
+            }
+
+            err = test_constants(t.expected_constants.clone(), bytecode.constants);
+            match err {
+                // error returned
+                Some(e) => errors.push(format!("testConstants failed: {}", e)),
+                // compiled succesfully
+                None => {}
+            }
+        }
+
+        assert!(errors.is_empty(), "\n{}", errors.join("\n"));
+    }
+
+    #[test]
+    fn test_integer_arithmetic() {
+        let tests = vec![CompilerTestCase {
+            input: "1 + 2".to_string(),
+            expected_constants: vec![ExpectedLiteral::Int(1), ExpectedLiteral::Int(2)],
+            expected_instructions: vec![
+                Instructions(code::make(&Opcode::OpConstant, &vec![0])),
+                Instructions(code::make(&Opcode::OpConstant, &vec![1])),
+            ],
+        }];
+
+        run_compiler_tests(&tests);
+    }
+}
